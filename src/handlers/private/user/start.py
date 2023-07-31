4bd25aef8fa35a6989f59def79c6f711a1fc602f
umarr <umarr.developer@gmail.com>
2023-07-31 14:06:52 +0600

    edit start handlers

--- src/handlers/private/user/start.py
+++ src/handlers/private/user/start.py
@@ -1,10 +1,26 @@
-from aiogram import Router, types
+from aiogram import F, Router, types
 from aiogram.filters import Command
+
+from src.models.user import User
 
 router = Router()
 
 
+async def menu(message: types.Message | types.CallbackQuery):
+    text = 'Выберите действие'
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+        [types.KeyboardButton(text='Перейти к тесту')],
+        [types.KeyboardButton(text='Таблица результатов'),
+         types.KeyboardButton(text='О боте')]
+    ])
+    await message.answer(text, reply_markup=keyboard)
+
+
 @router.message(Command(commands=['start']))
-async def on_start(message: types.Message):
-    text = 'Привет пользователь'
+async def on_start(message: types.Message, db, user: tuple[User]):
+    if not user:
+        user = await User.new(db, user_id=message.from_user.id)
+    text = f'Добро пожаловать, <b>{message.from_user.first_name}</b>'
     await message.answer(text)
+    await menu(message)
+
