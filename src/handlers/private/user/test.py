769eb0b5f50d30b777ce1b1598e7af57efec55ae
umarr <umarr.developer@gmail.com>
2023-08-08 14:43:58 +0600

    uodate test handlers

--- src/handlers/private/user/test.py
+++ src/handlers/private/user/test.py
@@ -1,10 +1,18 @@
 import asyncio
+
 from aiogram import F, Router, types
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.chat_action import ChatActionSender
-from src.models.user import User
+
+from src.models import Question, User
 
 router = Router()
+
+
+class Test(StatesGroup):
+    testing = State()
 
 
 @router.message(F.text == 'Перейти к тесту')
@@ -20,11 +28,30 @@ async def on_test(message: types.Message, db, user: tuple[User]):
 
 
 @router.callback_query(F.data == 'launch_test')
-async def on_laucnh_test(callback: types.CallbackQuery, bot):
+async def on_laucnh_test(callback: types.CallbackQuery, state: FSMContext, db, bot):
     text = 'Идет загрузка теста'
     await callback.message.edit_text(text)
 
-    async with ChatActionSender.typing(bot=bot, chat_id=callback.message.chat.id):
+    async with ChatActionSender.typing(chat_id=callback.message.chat.id, bot=bot):
         await asyncio.sleep(1)
-        text = 'Тест!'
+        text = 'Загрузка теста завершена'
+        questions = await Question.all(db)
+
+        await state.set_state(Test.testing)
+        await state.update_data({'questions': questions})
         await callback.message.answer(text)
+    
+    await on_test(callback, state)
+
+
+@router.callback_query()
+async def on_test(callback: types.CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    question = data['questions'][0][0]
+
+    description = question.description
+    answers = question.answers
+
+    text = f'Вопрос: {description}'
+    await callback.message.answer(text)
+    # TODO
